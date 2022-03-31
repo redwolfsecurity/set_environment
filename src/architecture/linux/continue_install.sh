9897ae57ade2210da13dc40308c01f55cfed479e
Dmitry Shevkoplyas <shevkoplyas@gmail.com>
2022-03-31 17:57:57 -0400

    This commit addressed the following issues: [x] Check which 7 functions aren't exported (export them all):   [     "discover_required_environment_variable",     "f",     "help_toc",     "is_shell_attribute_set",     "is_shell_option_set",     "s",     "trim_slash"   ]
    [x] Alphabetically sort some of the newly added set_environment functions.
    [x] Fix help_toc - remove unused code + must use ensure_variable_not_empty
    [x] Create a new f-n "file_toc()" to make similar table of contents for other set_environment files
    [x] Sort ./src/architecture/linux/continue_install.functions.sh (use new f-n file_toc)
    [x] Add ./src/architecture/linux/continue_install.functions.sh TOC
    [x] Replace misleading statement:
        # Note: the error details were already reported by ensure_variable_not_empty()
        And add set_state() before return 1;

--- src/architecture/linux/continue_install.sh
+++ src/architecture/linux/continue_install.sh
@@ -2,31 +2,32 @@
 
 # This is OS-family specific script to continue installing "set_environment" project.
 
-# Source OS-specific install functions
-source src/architecture/linux/continue_install.functions.sh || { error "Failed to source OS-specific install functions."; exit 1; }
-
 ###############################################################################
 #
 # continue_install() is a function to continuation OS-specific portion of set environment installation.
 #
 function continue_install {
+    # Source OS-specific install functions
+    source src/architecture/linux/continue_install.functions.sh || { set_state "${FUNCNAME[0]}" "terminal_error_cant_source_os_specific_install_functions"; abort; }
+    
     # Before installer change directory multiple times let's preserve absolute path to the project's root
     # directory, so we can preserve soruces as one of the last steps after installation.
     local PROJECT_ROOT_DIR="${PWD}"
+    [ ! -z "${PROJECT_ROOT_DIR}" ] || { set_state "${FUNCNAME[0]}" "terminal_error_cant_get_current_working_directory"; abort; }
 
     # Install basic components.
-    install_set_environment_baseline || { error "Failed to install_set_environment_baseline. Details: return code was: $?"; exit 1; }
-
+    install_set_environment_baseline || { set_state "${FUNCNAME[0]}" "terminal_error_failed_to_install_set_environment_baseline"; abort; }
+    
     # Project installer preserves the project source code: instead of erasing the source folder,
     # the code must be preserved under ff_agent/git/[project-owner]/set_environment/ folder.
-    preserve_sources "${PROJECT_ROOT_DIR}" || { error "Failed to preserve_sources. Details: return code was: $?"; exit 1; }
-
+    preserve_sources "${PROJECT_ROOT_DIR}" || { set_state "${FUNCNAME[0]}" "terminal_error_failed_to_preserve_source_code"; abort; }
+    
     # Preserved "set environment" sources provide the installer linked by set_environment_install script, which must be in the PATH.
-    ensure_ff_agent_bin_exists || { error "Failed to ensure_ff_agent_bin_exists. Details: return code was: $?"; exit 1; }
-    ensure_set_environment_install_exists || { error "Failed to ensure_set_environment_install_exists. Details: return code was: $?"; exit 1; }
+    ensure_ff_agent_bin_exists || { set_state "${FUNCNAME[0]}" "terminal_error_failed_to_ensure_ff_agent_bin_exists"; abort; }
+    ensure_set_environment_install_exists || { set_state "${FUNCNAME[0]}" "terminal_error_failed_to_ensure_set_environment_install_exists"; abort; }
 }
 
-continue_install
+continue_install  # no need to check errors here, the f-n itself reports errors (all of which are "terminal" errors) and aborts/exits.
 
 # Install additional components
 # TODO: if any arguments passed to installer, call corresponding installer (pass control to nodejs instller portion)
@@ -35,5 +36,3 @@ continue_install
 #   # node /some/path/to/node/instller/portion
 # fi
 
-# Report sucess
-set_state 'install_set_environment' 'success'
