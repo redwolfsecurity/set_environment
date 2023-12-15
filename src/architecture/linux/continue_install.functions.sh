079fcc284c09fc82931c74f8aedbe304d574aba0
Laser Tiger <anthony.ng@redwolfsecurity.com>
2023-12-15 12:34:27 -0500

    Improving node installation

--- src/architecture/linux/continue_install.functions.sh
+++ src/architecture/linux/continue_install.functions.sh
@@ -1080,27 +1080,16 @@ export -f install_n
 function install_nodejs {
   set_state "${FUNCNAME[0]}" 'started'
 
-  local APPROACH='n'
+  # Get desired nodejs version
+  local VERSION="$( get_desired_nodejs_version )"
+  [ -z "${VERSION}" ] && { set_state "${FUNCNAME[0]}" 'failed_to_get_desired_nodejs_version'; abort; }
 
-  # Get expected nodejs version
-  local VERSION="$( get_expected_nodejs_version )"
-  [ -z "${VERSION}" ] && { set_state "${FUNCNAME[0]}" 'failed_to_get_expected_nodejs_version'; abort; }
 
-  case ${APPROACH} in
-      nvm)
-          install_nvm_ubuntu
-          export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-          [ -s "${NVM_DIR}/nvm.sh" ] && \. "${NVM_DIR}/nvm.sh"
-          nvm install lts/*
-      ;;
-      n)
-          # # We need to stop pm2 before replacing location of nodejs, otherwise any pm2 command would faild
-          # stop_pm2
-          # uninstall_n_outside_ff_agent_home  || { set_state "${FUNCNAME[0]}" 'terminal_error_uninstall_n_outside_ff_agent_home'; abort; }
-          install_n || { set_state "${FUNCNAME[0]}" 'terminal_error_install_n'; abort; }
-          n install "${VERSION}" || { set_state "${FUNCNAME[0]}" 'terminal_error_switching_node_version'; abort; }
-      ;;
-  esac
+  # # We need to stop pm2 before replacing location of nodejs, otherwise any pm2 command would faild
+  # stop_pm2
+  # uninstall_n_outside_ff_agent_home  || { set_state "${FUNCNAME[0]}" 'terminal_error_uninstall_n_outside_ff_agent_home'; abort; }
+  install_n || { set_state "${FUNCNAME[0]}" 'terminal_error_install_n'; abort; }
+  n install "${VERSION}" || { set_state "${FUNCNAME[0]}" 'terminal_error_switching_node_version'; abort; }
 
   set_state "${FUNCNAME[0]}" 'success'
   return 0
